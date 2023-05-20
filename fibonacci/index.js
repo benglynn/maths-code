7c8a24b255c892bd06a00cffed3826a37cac8e21
Ben Glynn <ben@benglynn.com>
2023-05-20 18:52:00 +0100

    laid out fibonacci spiral

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -8,7 +8,7 @@ const svgEl = (name, attrs) => {
   return el;
 };
 
-const grid = 10;
+const grid = 4;
 
 const square = { rx: 2, ry: 2, class: "rect" };
 
@@ -17,17 +17,44 @@ const svg = document.body.appendChild(svgEl("svg", { class: "svg" }));
 let step = 0;
 let nMinusTwo = 1;
 let nMinusOne = 0;
-let limit = 4;
+let limit = 16;
+
+let x = 800;
+let y = 200;
 
 while (step <= limit) {
   const n = nMinusTwo + nMinusOne;
-  console.log(`Step ${step}: ${n.toLocaleString()}\n`);
+  console.log(
+    `Step ${step}: ${n.toLocaleString()} (${["N", "E", "S", "W"][step % 4]}])\n`
+  );
 
-  const x = grid + step * grid;
-  const y = grid + step * grid;
   const width = n * grid;
   const height = n * grid;
+
+  // advance position
+
+  switch (step % 4) {
+    case 0: // north
+      y -= n * grid;
+      break;
+    case 1: // east
+      x += nMinusOne * grid;
+      break;
+    case 2: // south
+      x -= nMinusTwo * grid;
+      y += nMinusOne * grid;
+      break;
+    case 3: // west
+      x -= n * grid;
+      y -= nMinusTwo * grid;
+      break;
+  }
+
+  // draw it
+
   svg.appendChild(svgEl("rect", { x, y, width, height, ...square }));
+
+  // next iteration
 
   nMinusTwo = nMinusOne;
   nMinusOne = n;
